1db756dc3c815cb8d7a3260511b223a74d528137
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-27 14:01:13 -0500

    Update header.h

--- header.h
+++ header.h
@@ -2,11 +2,11 @@
 #define header_H
 
 //change buffer size N amount
-
+const char* buffer_size(int N);
 //change bytes read to N from each file
-
+const char* change_bytes(int N);
 //apply Caesar cipher to k alphabetical characters
-
+const char* caesar_cipher(char msg[], int k);
 //general rotation shifting all characters k times
 
 //output data from file in hexadecimal format
