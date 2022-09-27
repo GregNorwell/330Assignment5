23613dc6138b424135270631e353fa217d503cc7
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-27 11:43:13 -0500

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,16 @@
+#ifndef header_H
+#define header_H
 
+//change buffer size N amount
+
+//change bytes read to N from each file
+
+//apply Caesar cipher to k alphabetical characters
+
+//general rotation shifting all characters k times
+
+//output data from file in hexadecimal format
+
+//output data from the file in binary notation
+
+#endif
