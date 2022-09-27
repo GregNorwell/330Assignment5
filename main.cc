660fff0b643fc08b40d813ffdf0cdc81442cbd47
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-27 11:32:18 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,26 @@
+#include <cstdio>
+
 #include "header.h"
 #include "functions.cc"
 int main(int argc, char* argv[]){
-  
+  switch(getopt(argc, argv, ":b:n:C:r:XB")){
+    case 'b':
+      break;
+    case 'n':
+      break;
+    case 'C':
+      break;
+    case 'r':
+      break;
+    case 'X':
+      break;
+    case 'B':
+      break;
+    case ':':
+      perror("option needs a value");
+      return(-1);
+    case '?':
+      perror("unknown option");
+      return(-1);
+  }
 }
