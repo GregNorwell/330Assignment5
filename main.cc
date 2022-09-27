cfdfeb77cec9284c4cbd9d6badca96c70f855099
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-27 14:01:16 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,26 +1,35 @@
 #include <cstdio>
 
-#include "header.h"
 #include "functions.cc"
 int main(int argc, char* argv[]){
-  switch(getopt(argc, argv, ":b:n:C:r:XB")){
-    case 'b':
-      break;
-    case 'n':
-      break;
-    case 'C':
-      break;
-    case 'r':
-      break;
-    case 'X':
-      break;
-    case 'B':
-      break;
-    case ':':
-      perror("option needs a value");
-      return(-1);
-    case '?':
-      perror("unknown option");
-      return(-1);
+  file = argv[optind];
+  int fd = open(file, O_RDONLY);
+  while((opt = getopt(argc, argv, ":b:n:C:r:XB")) != -1)
+  {
+    switch(opt)
+    {
+     case 'b':
+        //use optarg for argument
+        break;
+      case 'n':
+        break;
+      case 'C':
+        break;
+      case 'r':
+        break;
+      case 'X':
+        break;
+      case 'B':
+        break;
+      case ':':
+        perror("option needs a value");
+        return(-1);
+      case '?':
+        perror("unknown option");
+        return(-1);
+      default:
+        break;
+    }
   }
+  
 }
