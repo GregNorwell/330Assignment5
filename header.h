f9a84e549c4e0522df4402d00aadd1099d17807a
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-27 19:14:39 -0500

    Update header.h

--- header.h
+++ header.h
@@ -6,11 +6,11 @@ const char* buffer_size(int N);
 //change bytes read to N from each file
 const char* change_bytes(int N);
 //apply Caesar cipher to k alphabetical characters
-const char* caesar_cipher(char msg[], int k);
+const char* caesar_cipher(char* msg[], int k);
 //general rotation shifting all characters k times
-
+const char* general_rotation(char* msg[], int k);
 //output data from file in hexadecimal format
-
+const char* hex_conversion(char* msg[]);
 //output data from the file in binary notation
-
+const char* bin_conversion(char* msg[]);
 #endif
