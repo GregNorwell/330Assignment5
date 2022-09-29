7a8c8184307cb9d665ff0baeac112fdf07b75731
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-29 16:25:07 -0500

    Update header.h

--- header.h
+++ header.h
@@ -2,12 +2,12 @@
 #define header_H
 
 //apply Caesar cipher to k alphabetical characters
-void caesar_cipher(char msg[], int k);
+void caesar_cipher(char &msg[], int k);
 //general rotation shifting all characters k times
-void general_rotation(char msg[], int k);
+void general_rotation(char &msg[], int k);
 //output data from file in hexadecimal format
-void hex_conversion(char msg[], int limit);
+void hex_conversion(char &msg[], int limit);
 //output data from the file in binary notation
-void bin_conversion(char msg[], int limit);
+void bin_conversion(char &msg[], int limit);
 
 #endif
