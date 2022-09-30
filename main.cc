c05c1656dfb1993071922d944ce84463eab35169
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-09-29 19:09:57 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -8,17 +8,17 @@
 #include "header.h"
 int main(int argc, char* argv[]){
     bool hex, bin;
-    int n = 0; altBuffer = -1, altLength = -1, CC = -1, GR = -1;
+    int buffer = 1, length = -1, CC = -1, GR = -1;
     while((opt = getopt(argc, argv, ":b:n:C:r:XB")) != -1)
     {
      switch(opt)
      {
       case 'b':
          //use optarg for argument
-         altBuffer = atoi(optarg);
+         buffer = atoi(optarg);
          break;
        case 'n':
-         altLength = atoi(optarg);
+         length = atoi(optarg);
          break;
        case 'C':
          CC = atoi(optarg);
@@ -45,31 +45,37 @@ int main(int argc, char* argv[]){
   
   for(int i = optind; i < argc; i++)
   {
-    int *arr = calloc(n, sizeof(char));
+    int j;
+    char *arr = calloc(0, sizeof(char));
     if(arr == NULL){perror("Cannot allocate memory");return(-1);}
-    
     file = argv[i];
     int fd = open(file, O_RDONLY);
-    for(int i = 0; i < n; i++){
-      arr = realloc((arr, (n + 1) * sizeof(char));
-      arr[i] = read(fd, arr, n);
-    }
     
-    if(altBuffer != -1){
+    if(length != -1){
+        for(j = 0; j < length; j++){
+          arr = realloc(arr, (j + buffer) * sizeof(char));
+          arr[j] = read(fd, arr, buffer);
+        }
+    }else{
+        for(j = 0; arr[j] == '\0'; j++){
+          arr = realloc(arr, (j + buffer) * sizeof(char));
+          arr[j] = read(fd, arr, buffer);
+        }
     }
-    else if(altLength != -1){
-    }
-    else if(CC != -1){
-      caesar_cipher(file, CC);
+         
+    if(CC != -1){
+      caesar_cipher(arr, CC);
     }
     else if(GR != -1){
-      general_rotation(file, GR)
+      general_rotation(arr, GR);
     }
-    else if(hex == true){
+    if(hex == true){
+      hex_conversion(arr, j);
     }
     else if(bin == true){
+      bin_conversion(arr, j);
     }
-    write(STDOUT_FILENO, arr, n);
+    write(STDOUT_FILENO, arr, j);
     free(arr);
   }
   
