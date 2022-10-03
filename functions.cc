51518edad8de8d680fb5960e486e251ef565dbb5
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-10-03 18:51:03 -0500

    Update functions.cc

--- functions.cc
+++ functions.cc
@@ -42,12 +42,14 @@ void hex_conversion(char *& msg, int length)
   
   for(int i = 0; i < length; ++i){std::cout << temp[i] << " ";}
   std::cout << std::endl;
+  for(int i = 0; i < length; ++i){std::cout << msg[i] << " ";}
+  std::cout << std::endl;
   
   for(int i = 0; i < length; i++){
     msg[j] = temp[i] & 15;
-    std::cout << msg[j];
+    //std::cout << msg[j];
     msg[j + 1] = temp[i] >> 4;
-    std::cout << msg[j+ 1];
+    //std::cout << msg[j+ 1];
     j += 2;
   }
   for(int i = 0; i < (length * 2); ++i){std::cout << msg[i] << " ";}
