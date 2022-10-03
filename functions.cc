0e7635b5f28d544073a5ae277a7aa68b106f709e
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-10-03 18:22:17 -0500

    Update functions.cc

--- functions.cc
+++ functions.cc
@@ -37,7 +37,7 @@ void hex_conversion(char *& msg, int length)
   char *temp = new char[length * 2];
   int j = 0;
   
-  for(int i = 0; i < 5; ++i){std::cout << msg[i] << " ";}
+  for(int i = 0; i < length; ++i){std::cout << msg[i] << " ";}
   std::cout << length;
   
   for(int i = 0; i < length; i++){
