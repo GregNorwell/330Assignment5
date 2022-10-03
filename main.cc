82f006e685344e58dd851d0abdd4b1646cc0b1fc
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-10-03 17:42:53 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
     }
     for(int i = optind; i < argc; i++)
     {
-        int j, size;
+        int j;
         char *buffer = new char[arraySize];
         if(strcmp(argv[i], "-") == 0)
         {
