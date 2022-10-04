9b78e792f9947d705cbb98c7e350ba42a9df0fed
GregNorwell <111902417+GregNorwell@users.noreply.github.com>
2022-10-03 20:14:30 -0500

    Update functions.cc

--- functions.cc
+++ functions.cc
@@ -4,18 +4,18 @@
 #include <iostream>
 
 #include "header.h"
-void caesar_cipher(char *& msg, int k){ // does each word at a time
+void caesar_cipher(char *& msg, int k){ 
   char ch;
   for(int i = 0; msg[i] != '\0'; i++)
   {
     ch = msg[i];
-    if(ch >= 'a' && ch <= 'z')
+    if(ch >= 'a' && ch <= 'z')//lower case
     {
       ch = ch + k;
       if(ch > 'z'){ch = ch - 'z' + 'a' -1;}
       msg[i] = ch;
     }
-    else if (ch >= 'A' && ch <= 'Z')
+    else if (ch >= 'A' && ch <= 'Z')//uppercase
     {
       ch = ch + k;
       if(ch > 'Z'){ch = ch - 'Z' + 'A' -1;}
@@ -25,7 +25,7 @@ void caesar_cipher(char *& msg, int k){ // does each word at a time
 }
 void general_rotation(char *& msg, int k){
   char ch;
-  for(int i = 0; msg[i] != '\0'; i++)
+  for(int i = 0; msg[i] != '\0'; i++)//general rotation: work 255+
   {
     ch = msg[i];
     ch = ch + k;
@@ -37,15 +37,16 @@ void hex_conversion(char *& msg, int length)
   char *temp = new char[length];
   for(int i = 0; i < length; i++)
     temp[i] = msg[i];
+  static const char hex_digits[] = "0123456789ABCDEF";
   msg = (char*)realloc(msg, (length * 2) * sizeof(char));
   int j = 0;
   
   //for(int i = 0; i < length; ++i){std::cout << msg[i];}
   
   for(int i = 0; i < length; i++){
-    msg[j] = int(temp[i]) >> 4;
+    msg[j] = hex_digits[int(temp[i]) >> 4];
     std::cout << int(msg[j]);
-    msg[j + 1] = int(temp[i]) & 15;
+    msg[j + 1] = hex_digits[int(temp[i]) & 15];
     std::cout << int(msg[j+ 1]);
     j += 2;
   }
